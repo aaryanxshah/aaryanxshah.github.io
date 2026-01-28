1a4ea50a628448d6587fa024f985c6b24b3321ed
aaryanxshah <toaaryanshah@gmail.com>
2026-01-28 09:38:45 -0800

    content changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,37 +5,52 @@ export default function Home() {
       <p>Building at the intersection of technology and health.</p>
       <p>
         {" "}
-        My <a href="https://linkedin.com/in/aaryanxshah">LinkedIn</a> is mostly updated with my professional experience.
-        I am finishing my BS in Computer Science and MS in Biomedical Data Science at Stanford. I am currently working
-        at Canvas Medical as an engineer on the Applied AI team, working on agentic capabilities and clinical-grade AI
-        governance within our EHR.{" "}
+        My{" "}
+        <a href="https://linkedin.com/in/aaryanxshah" target="_blank">
+          LinkedIn
+        </a>{" "}
+        is mostly updated with my professional experience. I am finishing my BS in Computer Science and MS in Biomedical
+        Data Science at Stanford. I am currently working at Canvas Medical as an engineer on the Applied AI team,
+        working on agentic capabilities and clinical-grade AI governance within our EHR.{" "}
       </p>
       <p>
         {" "}
         When designing this page, I was in the middle of{" "}
-        <a href="https://www.stripe.press/poor-charlies-almanack">Poor Charlie's Almanack</a> and had recently listened
-        to the{" "}
-        <a href="https://www.acquired.fm/episodes/berkshire-hathaway-part-i">
+        <a href="https://www.stripe.press/poor-charlies-almanack" target="_blank">
+          Poor Charlie's Almanack
+        </a>{" "}
+        and had recently listened to the{" "}
+        <a href="https://www.acquired.fm/episodes/berkshire-hathaway-part-i" target="_blank">
           Berkshire Hathaway episode on the Acquired podcast
         </a>
-        . Also, selfishly, given the growing utilization (and dependence) of coding agents, it felt satisfying to make
-        this on my own with just web searches for HTML syntax.{" "}
+        . As a result, the{" "}
+        <a href="https://www.berkshirehathaway.com" target="_blank">
+          {" "}
+          Berkshire Hathaway website
+        </a>{" "}
+        played a rather instrumental role. Also, selfishly, given the growing utilization (and dependence) of coding
+        agents, it felt satisfying to make this on my own with just web searches for HTML syntax.{" "}
       </p>
       <hr />
-      <h2>Work</h2>
+      <h2>
+        Work (see{" "}
+        <a href="https://linkedin.com/in/aaryanxshah" target="_blank">
+          LinkedIn
+        </a>{" "}
+        for all work experiences, most notable below){" "}
+      </h2>
       <p>
-        <strong>Company One</strong>
+        <strong>Canvas Medical</strong>
         <br />
-        <em>Role, 2024</em>
+        <em>June 2025 - Present</em>
         <br />
         Short description of what you did here.
       </p>
       <p>
-        <strong>Company Two</strong>
+        <strong>Amgen</strong>
         <br />
-        <em>Role, 2023</em>
+        <em>June - September 2024</em>
         <br />
-        Short description of what you did here.
       </p>
       <hr />
       <h2>Research</h2>
@@ -93,10 +108,26 @@ export default function Home() {
       </p>
       <hr />
       <h2>About</h2>
-      <p>I'm from Fremont, CA and therefore am an avid Bay Area sports fan.</p>
+      <p>
+        I'm from Fremont, CA and therefore, am an avid Bay Area sports fan. I had a David Lee jersey in 2012 and I
+        consider that proof that I'm not a Warriors bandwagon.{" "}
+      </p>
+      <p>
+        [February 2026] Will try to update these but currently reading Poor Charlie's Almanack, have switched from a lot
+        of house / Beatles to the Acquired podcast, and am trying to get better at golf to use the Stanford golf course
+        at the discounted rates at least once.
+      </p>
+      <p>
+        <img src="/imgs/me.jpg" alt="Picture of Aaryan at Stanford's Camp Kesem in 2022" width={400}></img>
+      </p>
       <a href="mailto:aaryans@stanford.edu">Email</a> | <a href="https://github.com/aaryanxshah">GitHub</a> |{" "}
-      <a href="https://linkedin.com/in/aaryanxshah">LinkedIn</a> |{" "}
-      <a href="https://scholar.google.com/citations?user=sfw7ATcAAAAJ&hl=en&oi=ao">Google Scholar</a>
+      <a href="https://linkedin.com/in/aaryanxshah" target="_blank">
+        LinkedIn
+      </a>{" "}
+      |{" "}
+      <a href="https://scholar.google.com/citations?user=sfw7ATcAAAAJ&hl=en&oi=ao" target="_blank">
+        Google Scholar
+      </a>
       <hr />
       <p>&copy; 2026 Aaryan Shah</p>
     </>
---
+++ src/app/research/page.tsx
@@ -0,0 +1,8 @@
+export default function Research() {
+  return (
+    <>
+      <h1>Research</h1>
+      <p>I'm currently working on a few research projects.</p>
+    </>
+  );
+}
